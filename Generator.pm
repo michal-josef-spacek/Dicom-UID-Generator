116e015941d560e172f1a9a6f33481cd013a695c
Michal Špaček <m.spacek@ibsmm.com>
2016-08-25 11:23:31 +0200

    Add doc.

--- Generator.pm
+++ Generator.pm
@@ -99,3 +99,167 @@ sub _root_uid {
 1;
 
 __END__
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+Dicom::UID::Generator - DICOM UID generator.
+
+=head1 SYNOPSIS
+
+ use Dicom::UID::Generator;
+ my $obj = Dicom::UID::Generator->new(%params);
+ my $uid = $obj->create_series_instance_uid;
+ my $uid = $obj->create_sop_instance_uid;
+ my $uid = $obj->create_study_instance_uid;
+ my $uid = $obj->create_uid($prefix);
+
+=head1 METHODS
+
+=over 8
+
+=item C<new(%params)>
+
+ Constructor.
+
+=over 8
+
+=item * C<library_number>
+
+ DICOM library number.
+ Default value is undef.
+
+=item * C<model_number>
+
+ Device model number.
+ Default value is undef.
+
+=item * C<serial_number>
+
+ Device serial number.
+ Default value is undef.
+
+=item * C<timezone>
+
+ Time zone for time in UID..
+ Default value is 'Europe/Prague'.
+
+=item * C<uid_counter>
+
+ UID counter number for part of final UID.
+ Default value is 0.
+
+=back
+
+=item C<create_series_instance_uid()>
+
+ Get DICOM Series Instance UID.
+ Returns string.
+
+=item C<create_sop_instance_uid()>
+
+ Get DICOM SOP Instance UID.
+ Returns string.
+
+=item C<create_study_instance_uid()>
+
+ Get DICOM Study Instance UID.
+ Returns string.
+
+=item C<create_uid($prefix)>
+
+ Get DICOM UID defined by prefix.
+ Returns string.
+
+=back
+
+=head1 ERRORS
+
+ new():
+         From Class::Utils::set_params():
+                 Unknown parameter '%s'.
+
+=head1 EXAMPLE
+
+ # Pragmas.
+ use strict;
+ use warnings;
+
+ # Modules.
+ use Dicom::UID::Generator;
+
+ # Object.
+ my $obj = Dicom::UID::Generator->new(
+       'library_number' => 999,
+       'model_number' => '001',
+       'serial_number' => 123,
+ );
+
+ # Get Series Instance UID.
+ my $series_instance_uid = $obj->create_series_instance_uid;
+
+ # Get Study Instance UID.
+ my $study_instance_uid = $obj->create_study_instance_uid;
+
+ # Get SOP Instance UID.
+ my $sop_instance_uid = $obj->create_sop_instance_uid;
+
+ # Print out.
+ print "Study Instance UID: $study_instance_uid\n";
+ print "Series Instance UID: $series_instance_uid\n";
+ print "SOP Instance UID: $sop_instance_uid\n";
+
+ # Output like:
+ # Study Instance UID: 999.001.123.1.2.976.20160825112022726.2
+ # Series Instance UID: 999.001.123.1.3.976.20160825112022647.1
+ # SOP Instance UID: 999.001.123.1.4.976.20160825112022727.3
+
+ # Comments:
+ # 999 is DICOM library number.
+ # 001 is device model number.
+ # 123 is device serial number.
+ # 1.2, 1.3, 1.4 are hardcoded resolutions of DICOM UID type.
+ # 976 is PID of process.
+ # 20160825112022726 is timestamp.
+ # last number is number of 'uid_counter' parameter.
+
+=head1 DEPENDENCIES
+
+L<Class::Utils>,
+L<DateTime::HiRes>,
+L<English>,
+L<Readonly>,
+
+=head1 SEE ALSO
+
+=over
+
+=item L<Task::Dicom>
+
+Install the Dicom modules.
+
+=back
+
+=head1 REPOSITORY
+
+L<https://github.com/tupinek/Dicom-UID-Generator>
+
+=head1 AUTHOR
+
+Michal Špaček L<mailto:skim@cpan.org>
+
+L<http://skim.cz>
+
+=head1 LICENSE AND COPYRIGHT
+
+ © Michal Špaček 2016
+ BSD 2-Clause License
+
+=head1 VERSION
+
+0.01
+
+=cut
